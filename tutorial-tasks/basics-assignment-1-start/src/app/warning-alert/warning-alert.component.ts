6fd2d5909bb92c7e2f69a1e313428ef80fa6992b
szacharek <sylwia.zacharek@solwit.com>
2019-10-04 10:24:37 +0200

    lesson 24th and before

---
+++ tutorial-tasks/basics-assignment-1-start/src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: "app-warning-alert",
+  templateUrl: "./warning-alert.component.html",
+  styles: [`
+    h3 {
+      color: red;
+      font-weight: bold;
+    }
+  `]
+})
+export class WarningAlertComponent{
+
+}
