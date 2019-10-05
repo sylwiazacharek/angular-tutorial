dd327fd4edd877f87650c15900002c493662cc32
sylwiazacharek <zelek.sylwia94@gmail.com>
2019-10-05 18:33:01 +0200

    lessons 59-66 - databinding example

---
+++ tutorial-tasks/cmp-databinding-start/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  serverElements = [
+  {
+    type: 'server',
+    name: 'TestServer',
+    content: 'Just a test'
+  },
+  {
+    type: 'server1',
+    name: 'TestServer1',
+    content: 'Just a test1'
+  }];
+
+}
